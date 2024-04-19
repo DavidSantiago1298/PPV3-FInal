96dc54064b8624e9480dcc6aba2fc5cfec08bb73
Daviduwuwuwu <156475324+DavidSantiago1298@users.noreply.github.com>
2024-04-19 12:02:47 -0600

    Update Leccion.cs

--- Duolingo-PPV3/Assets/Scripts/Leccion.cs
+++ Duolingo-PPV3/Assets/Scripts/Leccion.cs
@@ -1,15 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-
-
 //En este scrypt se guarda la informacion de la leccion a la vez que la almacena para usarse en el juego
 
 [System.Serializable]
 public class Leccion
 {
-    //Este metodo crea el numero de leccion
+    //Este metodo crea el numero ID de leccion
     public int ID;
     //Este metodo crea la pregunta
     public string lessons;
